53d0d77d2f56901b884a6a0076e97e0c77f266b5
Виталий <Dantic004@gmail.com>
2024-09-05 17:10:57 +0300

    feature-2 homework conditions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
-        System.out.print("Hello world!");
-        System.out.print("Hello world!");
+
+
+        byte countRunCycleForField; //объявление переменой
+        countRunCycleForField = 127; //инициализация переменой
+
+        byte countRunCycleForField2 = 122;
+        short myShort = 127;  // camel case
+        short my_short = 127; //snake case напримур SQL
+
+        int myInt = 2_000_000_000;
+        long myLong = 3_000_000_000L;
+
+        float myFloat = 3.0f;
+        double myDouble = 3.0;
+
+        char myChar = 'a';
+
+        boolean myBoolean = false;
+
+
+
+
+
+        int i = 100;
+        double d = 28.3;
+        byte b =60;
+        float f= 13;
+        short s = 52;
+        long l = 14;
+        char a = 120;
+        boolean c = i > b;
+
+        int Sum = (int) (i+ d);          // сложение
+        int difference = (int) (b - f); //Вычитание
+        int product = (int) (s * l);    //умножение
+        int quotient = a / b;          //Деление
+        int remainder = b % s;        //Остаток от деления
+
+        System.out.println("сложение (double): " + Sum);
+        System.out.println("Вычитание (double): " + difference);
+        System.out.println("умножение (double): " + product);
+        System.out.println("Деление (double): " + quotient);
+        System.out.println("Остаток от деления (double): " + remainder);
+        System.out.println(c);
+
+            }
     }
-}
